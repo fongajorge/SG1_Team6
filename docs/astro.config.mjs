af1fe2e3c3b3895af745c1dd734176dcbc880ea7
jfong088 <0249352@up.edu.mx>
2026-05-26 07:44:38 -0600

    Fix deployment for GitHub Pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Replace this with your actual GitHub username and repository name!
+    site: 'https://fongajorge.github.io',
+    base: 'SG1_Team6/',
+});
